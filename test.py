ee3e2a1e13e02ae283ce21f23d9d54d1a652505e
ambreetienne <ambre.etienne@ensae.fr>
2022-12-04 15:44:02 +0100

    Premiers tests

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-import numpy as np
+#%%
+import urllib
+import bs4
+import pandas
+from urllib import request
+import requests
 
-def fonction(x):
-    return x
+#%%
+url = 'http://sofifa.com/players?offset=0'
+# %%
+r = requests.get(url)
 
+# %%
+markup = r.content
+
+# %%
+soup = bs4.BeautifulSoup(markup, 'lxml')
+
+
+# %%
+table = soup.find('table', {'class': 'table-hover'})
+# %%
+tbody = table.find('tbody')
+# %%
+all_a = tbody.find_all('a', {'class': ''})
+# %%
+[‘http://sofifa.com' + player[‘href’] for player in all_a]
+# %%
