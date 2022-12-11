f328efc245d878bdd692b731cc44a550964e1eff
ambreetienne <ambre.etienne@ensae.fr>
2022-12-11 16:26:55 +0100

    commit

--- test.py
+++ test.py
@@ -7,188 +7,27 @@ from urllib import request
 import requests
 
 #%%
-url = 'http://sofifa.com/players?offset=0'
-
+def convert_value(wage):
+    a=wage.split('€')
+    if len(a[1].split('K'))==2:
+        b=a[1].split('K')
+        return float(b[0])*1000
+    elif len(a[1].split('M'))==2:
+        b=a[1].split('M')
+        return float(b[0])*1000000
+    else:
+        return float(a[1])
 # %%
-# Fonction génératrice de page html
-def soup_maker(url):
-    r = requests.get(url)
-    markup = r.content
-    soup = bs4.BeautifulSoup(markup, 'lxml')
-    return soup
+def convert_age(age):
+    a=age.split('y')
+    return int(a[0])
 
 #%%
-#Fonction donnant la liste des url de chaque n*60 joueur
-def all_ref(n):
-    L=[]
-    for i in range(n):
-        url='http://sofifa.com/players?offset=' + str(i*60)
-        soup=soup_maker(url)
-        table = soup.find('table', {'class': 'table-hover'})
-        tbody = table.find('tbody', {'class': 'list'})
-        col_name = tbody.find_all('tr')
-        for col in col_name:
-            col_ref = col.find('a')
-            L.append(col_ref['href'])
-    return ["http://sofifa.com" + L[i] for i in range(len(L))]
-
-
-# %%
-#Fonction donnant le nom d'un joueur à partir de son url
-def name (url):
-    soup = soup_maker(url)
-    info=soup.find('div', {'class': "col-12"})
-    return info.find('h1').text.strip()
-
-# %% 
-#Fonction donnant l'âge d'un joueur
-def age (url):
-    soup = soup_maker(url)
-    info=soup.find('div', {'class': "col-12"})
-    bio = info.find('div', {'class': 'meta ellipsis'})
-    bio_str = bio.text.strip()
-    bio = bio_str.split()
-    return bio[-6]
+def convert_poids(poids):
+    a=poids.split('k')
+    return int(a[0])
 
 #%%
-#Fonction donnant le poids d'un joueur
-def poids(url):
-    soup = soup_maker(url)
-    info=soup.find('div', {'class': "col-12"})
-    bio = info.find('div', {'class': 'meta ellipsis'})
-    bio_str = bio.text.strip()
-    bio = bio_str.split()
-    return bio[-1]
-
-#%%
-#Fonction donnant la taille d'un joueur
-def taille(url):
-    soup = soup_maker(url)
-    info=soup.find('div', {'class': "col-12"})
-    bio = info.find('div', {'class': 'meta ellipsis'})
-    bio_str = bio.text.strip()
-    bio = bio_str.split()
-    return bio[-2]
-
-# %%
-# Fonction donnant overall_rating d'un joueur
-def overall_rating(url):
-    soup=soup_maker(url)
-    info=soup.find('div', {'class': "bp3-card"})
-    info_stat=info.find('section', {'class': 'card spacing'})
-    all=info_stat.findAll('div', {'class': 'block-quarter'})
-    a=all[0].find('span')
-    return a.text.strip()
-
-#%% 
-# Fonction donnant potential d'un joueur
-def potential(url):
-    soup=soup_maker(url)
-    info=soup.find('div', {'class': "bp3-card"})
-    info_stat=info.find('section', {'class': 'card spacing'})
-    all=info_stat.findAll('div', {'class': 'block-quarter'})
-    a=all[1].find('span')
-    return a.text.strip()
-
-#%% 
-# Fonction donnant la valeur d'un joueur
-def value(url):
-    soup=soup_maker(url)
-    info=soup.find('div', {'class': "bp3-card"})
-    info_stat=info.find('section', {'class': 'card spacing'})
-    all=info_stat.findAll('div', {'class': 'block-quarter'})
-    a=all[2].find('div')
-    b=a.text.strip()
-    return b.split('V')[0]
-
-#%% 
-# Fonction donnant le salaire d'un joueur
-def wage(url):
-    soup=soup_maker(url)
-    info=soup.find('div', {'class': "bp3-card"})
-    info_stat=info.find('section', {'class': 'card spacing'})
-    all=info_stat.findAll('div', {'class': 'block-quarter'})
-    a=all[3].find('div')
-    b=a.text.strip()
-    return b.split('W')[0]
-
-#%%
-# Fonction donnant la valeur de la clause de rupture
-def release_clause(url):
-    soup=soup_maker(url)
-    info=soup.find('ul', {'class': 'pl'})
-    info_sup=info.findAll('li', {'class': 'ellipsis'})
-    a=info_sup[-2].find('span')
-    return a.text.strip()
-
-#%%
-# Fonction donnant la position de jeu d'un joueur
-def position(url):
-    oup=soup_maker(url)
-    info=soup.find('ul', {'class': 'ellipsis pl'})
-    info_pos=info.findAll('li')
-    a=info_pos[-4].find('span')
-    return a.text.strip()
-
-#%%
-def attacking(url):
-    soup=soup_maker(url)
-    skills=soup.findAll('div', {'class':'col col-12'})
-    attacking=skills[1].findAll('ul', {'class':'pl'})[0]
-    all=attacking.findAll('span',{'class': 'bp3-tag'})
-    L=[int(all[k].text.strip()) for k in range(5)]
-    return np.mean(L)
-
-# %%
-def technique(url):
-    soup=soup_maker(url)
-    skills=soup.findAll('div', {'class':'col col-12'})
-    attacking=skills[1].findAll('ul', {'class':'pl'})[1]
-    all=attacking.findAll('span',{'class': 'bp3-tag'})
-    L=[int(all[k].text.strip()) for k in range(5)]
-    return np.mean(L)
-
-#%%
-def mouvement(url):
-    soup=soup_maker(url)
-    skills=soup.findAll('div', {'class':'col col-12'})
-    attacking=skills[1].findAll('ul', {'class':'pl'})[2]
-    all=attacking.findAll('span',{'class': 'bp3-tag'})
-    L=[int(all[k].text.strip()) for k in range(5)]
-    return np.mean(L)
-
-#%%
-def puissance(url):
-    soup=soup_maker(url)
-    skills=soup.findAll('div', {'class':'col col-12'})
-    attacking=skills[1].findAll('ul', {'class':'pl'})[3]
-    all=attacking.findAll('span',{'class': 'bp3-tag'})
-    L=[int(all[k].text.strip()) for k in range(5)]
-    return np.mean(L)
-
-#%%
-def etat_esprit(url):
-    soup=soup_maker(url)
-    skills=soup.findAll('div', {'class':'col col-12'})
-    attacking=skills[1].findAll('ul', {'class':'pl'})[4]
-    all=attacking.findAll('span',{'class': 'bp3-tag'})
-    L=[int(all[k].text.strip()) for k in range(6)]
-    return np.mean(L)
-
-#%%
-def defense(url):
-    soup=soup_maker(url)
-    skills=soup.findAll('div', {'class':'col col-12'})
-    attacking=skills[1].findAll('ul', {'class':'pl'})[5]
-    all=attacking.findAll('span',{'class': 'bp3-tag'})
-    L=[int(all[k].text.strip()) for k in range(3)]
-    return np.mean(L)
-
-#%%
-def gardien(url):
-    soup=soup_maker(url)
-    skills=soup.findAll('div', {'class':'col col-12'})
-    attacking=skills[1].findAll('ul', {'class':'pl'})[6]
-    all=attacking.findAll('span',{'class': 'bp3-tag'})
-    L=[int(all[k].text.strip()) for k in range(5)]
-    return np.mean(L)
+def convert_taille(taille):
+    a=taille.split('c')
+    return int(a[0])
