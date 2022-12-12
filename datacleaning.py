1104d14652c09a356bf050304152f0677bcd4adb
ambreetienne <ambre.etienne@ensae.fr>
2022-12-12 14:24:24 +0100

    dataclean

--- datacleaning.py
+++ datacleaning.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 from urllib import request
 import requests
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # %%
 def convert_age(age):
@@ -37,21 +39,41 @@ def convert_value(wage):
 # %%
 df=pd.read_excel('Datasofifa.xlsx')
 #%%
-df.drop(columns=['Unnamed: 0'],axis=1)
+df=df.drop(columns=['Unnamed: 0'],axis=1)
 #%%
-df.dropna()
+df=df.dropna()
 #%%
 df=df.drop(df[df['Clause de rupture'] == 'Yes'].index)
 df=df.drop(df[df['Clause de rupture'] == 'No'].index)
 df=df.drop(df[df['Clause de rupture'] == 'N/A/ N/A'].index)
 
 #%%
-df.reset_index()
+df=df.reset_index()
 # %%
 df['Valeur']=df['Valeur'].apply(convert_value)
 df['Salaire']=df['Salaire'].apply(convert_value)
 df['Clause de rupture']=df['Clause de rupture'].apply(convert_value)
 
+#%%
+df['Age']=df['Age'].apply(convert_age)
+df['Taille']=df['Taille'].apply(convert_taille)
+df['Poids']=df['Poids'].apply(convert_poids)
 # %%
 df['Poste gardien']=np.where(df['Position']=='GK','1','0')
+
 # %%
+df.to_excel('Datasofifaclean.xlsx')
+
+# %%
+from sklearn import preprocessing
+
+#%%
+df['age_standard'] = preprocessing.scale(df['Age'])
+f, axes = plt.subplots(2, figsize=(10, 10))
+sns.distplot(df["Age"] , color="skyblue", ax=axes[0])
+sns.distplot(df["age_standard"] , color="olive", ax=axes[1])
+# %%
+df['poids_standard'] = preprocessing.scale(df['Poids'])
+f, axes = plt.subplots(2, figsize=(10, 10))
+sns.distplot(df["Poids"] , color="skyblue", ax=axes[0])
+sns.distplot(df["poids_standard"] , color="olive", ax=axes[1])
