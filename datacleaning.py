9a89e9530c49dd9e526bb6ab1c57aa14bf3a55a3
ambreetienne <ambre.etienne@ensae.fr>
2022-12-21 18:04:28 +0100

    mise au propre

--- datacleaning.py
+++ datacleaning.py
@@ -1,30 +1,27 @@
 #%%
-import urllib
-import bs4
 import pandas as pd
 import numpy as np
-from urllib import request
-import requests
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # %%
+# Fonction convertissant l'age en int
 def convert_age(age):
     a=age.split('y')
     return int(a[0])
 
 #%%
+#Fonction convertissant le poids en int
 def convert_poids(poids):
     a=poids.split('k')
     return int(a[0])
 
 #%%
+# Fonction convertissant la taille en int
 def convert_taille(taille):
     a=taille.split('c')
     return int(a[0])
 
 # %%
-# Fonction permettant de transformer les str en int
+# Fonction permettant de transformer les valeurs en int
 def convert_value(wage):
     a=wage.split('€')
     if len(a[1].split('K'))==2:
@@ -37,12 +34,14 @@ def convert_value(wage):
         return float(a[1])
 
 # %%
+# Importation de la dataframe
 df=pd.read_excel('Datasofifa.xlsx')
 #%%
 df=df.drop(columns=['Unnamed: 0'],axis=1)
 #%%
 df=df.dropna()
 #%%
+# On enlève les joueurs qui n'ont pas de clause de rupture
 df=df.drop(df[df['Clause de rupture'] == 'Yes'].index)
 df=df.drop(df[df['Clause de rupture'] == 'No'].index)
 df=df.drop(df[df['Clause de rupture'] == 'N/A/ N/A'].index)
@@ -50,30 +49,20 @@ df=df.drop(df[df['Clause de rupture'] == 'N/A/ N/A'].index)
 #%%
 df=df.reset_index()
 # %%
+# On convertit les valeurs en int grâce à la fonction prévue à cet effet
 df['Valeur']=df['Valeur'].apply(convert_value)
 df['Salaire']=df['Salaire'].apply(convert_value)
 df['Clause de rupture']=df['Clause de rupture'].apply(convert_value)
 
 #%%
+# On convertit l'âge, la taille et le poids en int grâce à la fonction prévue à cet effet
 df['Age']=df['Age'].apply(convert_age)
 df['Taille']=df['Taille'].apply(convert_taille)
 df['Poids']=df['Poids'].apply(convert_poids)
 # %%
+# On crée une nouvelle variable Poste gardien qui vaut 1 si le joueur est un gardien
 df['Poste gardien']=np.where(df['Position']=='GK','1','0')
 
 ##%%
-df.to_excel('Datasofifa.xlsx')
-
-# %%
-from sklearn import preprocessing
-
-#%%
-df['age_standard'] = preprocessing.scale(df['Age'])
-f, axes = plt.subplots(2, figsize=(10, 10))
-sns.distplot(df["Age"] , color="skyblue", ax=axes[0])
-sns.distplot(df["age_standard"] , color="olive", ax=axes[1])
-# %%
-df['poids_standard'] = preprocessing.scale(df['Poids'])
-f, axes = plt.subplots(2, figsize=(10, 10))
-sns.distplot(df["Poids"] , color="skyblue", ax=axes[0])
-sns.distplot(df["poids_standard"] , color="olive", ax=axes[1])
+# On exporte la base de données maintenant nettoyée au format excel
+df.to_excel('Datasofifaclean.xlsx')
